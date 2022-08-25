be52757deccd9d55ddaa91cabbc3585b4b70352c
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-08-26 04:32:15 +0530

    feat: updated pattern questions

--- youtube/RecursionByStriver/src/pattern.java
+++ youtube/RecursionByStriver/src/pattern.java
@@ -5,10 +5,61 @@ public class pattern {
             
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
-           for (int i = 1; i < n; i++) {
-            for (int j = 1; j < i+1; j++) { // i = 1, j < 2 -
-                  System.out.print(j+"");
-            }
+      //      for (int i = 1; i < n; i++) {
+      //       for (int j = 1; j < i+1; j++) { // i = 1, j < 2 -
+      //             System.out.print(j+"");
+      //       }
+//   ------------------------------------------------------------
+      //     pattern 2
+      //      for (int i = 1; i < n; i++) {
+      //       for (int j = 1; j < i+1; j++) { // i = 1, j < 2 -
+      //             System.out.print(i+"");
+      //       }
+
+//      // -----------------------------------------
+        //pattern 3
+            // for (int i = 0; i < n; i++) {
+            // for (int j = i; j < n; j++) { 
+            //       System.out.print("*"+"");
+            // }
+
+
+  // -----------------------------------------
+      // //   //pattern 4
+      //   for (int i = 0; i <n; i++) {
+      //       for (int j = 1; j <=n-i; j++) { 
+      //             System.out.print(j+"");
+      //       }
+
+
+ //_________-------------------------------------___________________________________________________
+//    //   //pattern 5
+//    for (int i = 0; i <n; i++) {
+//       for (int j = 1; j <=n-i; j++) { 
+//             System.out.print(j+"");
+//       }
+
+//-----------------------------------------------------------
+
+ //   //pattern 5
+//  for (int i = 0; i <n; i++) {
+//       for (int j = 1; j <=n-i; j++) { 
+//             System.out.print(j+"");
+//       }
+
+//---------------------------------------------------------
+      for (int i = 0; i < n; i++) {
+      for (int j = i; j < n; j++) { 
+            System.out.print("*"+"");
+      }
+
+
+
+
+
+
+
+
             System.out.println();
            } 
       }
