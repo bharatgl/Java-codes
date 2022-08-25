c0fddec3458a8d215a75db41c8274cc6388104a5
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-08-25 23:46:16 +0530

    123

--- youtube/RecursionByStriver/src/pattern.java
+++ youtube/RecursionByStriver/src/pattern.java
@@ -5,9 +5,9 @@ public class pattern {
             
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
-           for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                  System.out.print("*"+"");
+           for (int i = 1; i < n; i++) {
+            for (int j = 1; j < i+1; j++) { // i = 1, j < 2 -
+                  System.out.print(j+"");
             }
             System.out.println();
            } 
